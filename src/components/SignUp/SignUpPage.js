057540ac91d0b1b39832e266087f4cb9bdb556a5
Tristan vollenhoven <101865401+Tristan-10111@users.noreply.github.com>
2022-09-20 12:36:38 +0200

    made a few adaptive changes to the signup page

--- src/components/SignUp/SignUpPage.js
+++ src/components/SignUp/SignUpPage.js
@@ -1,25 +1,27 @@
 import React from 'react'
-
+import SignUpImg from '../../images/Remote meeting-cuate 1.png'
+import {Link} from 'react-router-dom'
+import './SignUpPage.css'
 
  function SignUpPage() {
   return (
- <div className='container d-flex justify-content-around mt-4'>
-   <div className='card mt-4 border border-0 h-75'>
+ <div className='container d-flex justify-content-around mt-5'>
+   <div className='card mt-5 border border-0 h-75'>
      <div className='card-body text-center d-flex flex-column align-items-center'>
        <h1 className='card-title fw-bold text-wrap'>Welcome Younglings!</h1>
 
       {/* sign up Inputs */}
        <div className="w-75 mt-4 d-flex flex-column"  >
 		    	<div className='input-group mb-3'style={{witdh:'2000px'}}>
-		    		<input type="email" className='form-control p-2 fw-400 fs-5' placeholder='Email' required />
+		    		<input type="email" className='form-control p-2 fw-semibold fs-5' placeholder='Email' required />
 		    	</div>
 
 			    <div className='input-group mb-3'>
-			    	<input type='password' className='form-control p-2 fw-400 fs-5' placeholder='Name' />
+			    	<input type='password' className='form-control p-2 fw-semibold fs-5' placeholder='Name' />
 			    </div>
 
 			    <div className='input-group mb-2'>
-			    	<input type='password' className='form-control p-2 fw-400 fs-5' placeholder='Password' />
+			    	<input type='password' className='form-control p-2 fw-semibold fs-5' placeholder='Password' />
 			    </div>
 		   </div>
 
@@ -27,19 +29,16 @@ import React from 'react'
        <div className='d-grid mt-4 w-75'>
         <button type='submit' className='btn btn-success p-2 fw-bold fs-4'>Sign up</button>
        </div>
-      
 
-       <div className='text-left mt-3 bottom-text fw-semibold smallText'style={{marginLeft:'15%'}}>
-         <span><a href="/" style={{
-           textDecoration:'none',
-           color:'#008800',
-         }}>Login</a></span> to an existing account
-       </div>
+       <Link to='/' className='link2 fw-semibold'>
+         <span>Login</span> to an existing account
+       </Link>
+
      </div>
    </div>
 
    <div className='Signupmage mt-4' >
-     <img src='/images/yportal.png' alt='SignupImage' />
+     <img src={SignUpImg} alt='SignupImage' />
    </div>
  </div>
  )
