b1609d4c05bbb871749e553f3942c3793ce46514
tristan vollenhoven <vollenhoventristan498@gmail.com>
2022-09-21 14:21:49 +0200

    made changes to navbar

--- src/components/NavigationBar/Navbar.js
+++ src/components/NavigationBar/Navbar.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav className=" container-fluid bg-tranparent d-flex">
-    <Link className="text-decoration-none fw-bold fs-2">Younglings</Link>
-      <input className="form-control w-25" type="search" placeholder="Search" />
+    <nav className=" container bg-tranparent d-flex">
+    <Link className="text-decoration-none fw-bold fs-1">Younglings</Link>
+      {/* <input className="form-control w-25" type="search" placeholder="Search" /> */}
       {/* <button className="btn btn-outline-success" type="submit">Search</button> */}
 </nav>
   )
